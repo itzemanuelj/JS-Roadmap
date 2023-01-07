e266c6c34a5844c96f39b86f4102c4d801c15898
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-07 14:05:21 -0500

    mvp commit for day 1
    added files for section 1 of javascript roadmap

---
+++ 1.Introduction To Javascript/1.what is javascript.js
@@ -0,0 +1,3 @@
+/// javascript is a server side and a client side scripting language
+/// client side is like the front endin runnning javascript to the browser
+/// sever is like the backlend of a website using node or deno to serve data
