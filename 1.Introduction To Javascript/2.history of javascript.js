3f7814fdbbd6f88b89e859546d737eb21a2b6a03
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-11 12:40:17 -0500

    Update 2.history of javascript.js

---
+++ 1.Introduction To Javascript/2.history of javascript.js
@@ -0,0 +1,22 @@
+/// 1)1990 internet begin .
+/// 2)1993=> Tim berners lee write html.
+/// 3)1993=> netscape browser lauch which was paid.
+/// 4)1995=>in 10days Brendan Eich build javascript. build as name Mocha while lauch
+/// as livescript and market as javascript by netscape due to popularity of java at
+/// that time.
+/// 5)Microsoft reverse engineer javascript and make Jscript which
+/// lauch in IE(Internet Explorer). In this way two two form of javascript netscape javascript and microsoft jscript.
+/// 6)Microsoft win as it was free and due to window.
+/// 7)Netscape go to Ecma International and request to make javascript standard.
+/// 8)1997=> Ecma international make EcmaScript 1
+/// 9)1999 dotcom bubble and netscape go yo loss and sold to AOL at 4.5 billion.
+/// 10)1999=> we got 3 version upto Ecmascript 3 .
+/// 11)2005=> Ajax introduction. Change of data in website without going to another page become possible,
+/// 12)2006 => jquery release and become famous.
+/// 13)2008=> Google come with chrome with V8 engine and it come with
+/// Just in time compilation.
+/// 14)May 2009=> we got node.js
+/// 15)december 2009=> EcmaScript 5 release.
+/// 16) 17 june 2015 => EsmaScript 6 release
+
+//https://www.codegrepper.com/profile/shirshak
