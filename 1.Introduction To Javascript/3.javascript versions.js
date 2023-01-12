5d4d24689c5f1e169a624a9ac65fcd7711cb3a1f
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-12 12:08:29 -0500

    Create 3.javascript versions.js

---
+++ 1.Introduction To Javascript/3.javascript versions.js
@@ -0,0 +1,5 @@
+/// The Original JavaScript ES1 ES2 ES3 (1997-1999)
+/// The First Main Revision ES5 (2009)
+/// The Second Revision ES6 (2015) Yearly Additions (2016, 2017, 2018, 2019, 2020)
+
+/// https://www.w3schools.com/js/js_versions.asp
